7f5b5afbd2131a9a2fe1cc15241eb6da2e85c8c2
siva durga mahesh adapa <71487096+sivaadapa-msit@users.noreply.github.com>
2020-12-24 08:41:12 +0530

    commit2

---
+++ src/conway/JunitTestConwaysBoard.java
@@ -0,0 +1,14 @@
+package conway;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class JunitTestConwaysBoard {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/conway/generateNextGeneration.java
@@ -0,0 +1,5 @@
+package conway;
+
+public class generateNextGeneration {
+
+}
